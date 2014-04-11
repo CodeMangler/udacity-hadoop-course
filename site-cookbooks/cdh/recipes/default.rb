8603209a9e24453288777eff2c6e40515a47833b
CodeMangler <hsdpal@gmail.com>
2014-04-11 22:48:30 +0530

    Configuration files, to get a barebones version running. Shell script to conveniently run Hadoop streaming jobs.

--- site-cookbooks/cdh/recipes/default.rb
+++ site-cookbooks/cdh/recipes/default.rb
@@ -21,3 +21,59 @@ end
     action :upgrade
   end  
 end
+
+template '/etc/hadoop/conf/core-site.xml' do
+  source 'core-site.xml.erb'
+end
+
+template '/etc/hadoop/conf/hdfs-site.xml' do
+  source 'hdfs-site.xml.erb'
+end
+
+template '/etc/hadoop/conf/mapred-site.xml' do
+  source 'mapred-site.xml.erb'
+end
+
+template '/usr/local/bin/hs' do
+  source 'hs.erb'
+end
+
+directory '/var/lib/hadoop-hdfs/cache/hdfs/dfs/name' do
+ owner 'hdfs'
+ group 'hadoop'
+ mode 00744
+ action :create
+ recursive true
+end
+
+file '/usr/local/bin/hs' do
+  mode 00755
+end
+
+%w(hadoop-0.20-mapreduce-jobtracker hadoop-0.20-mapreduce-tasktracker hadoop-hdfs-datanode hadoop-mapreduce-historyserver hadoop-yarn-nodemanager hadoop-yarn-resourcemanager).each do |service_name|
+  service service_name do
+   supports restart: true, start: true, stop: true, status: true
+   action [:enable, :start]
+  end
+end
+
+script "format namenode" do
+ interpreter "bash"
+ user "hdfs"
+ code <<-SCRIPT
+hdfs namenode -format -force
+ SCRIPT
+end
+
+service 'hadoop-hdfs-namenode' do
+ supports restart: true, start: true, stop: true, status: true
+ action [:enable, :start]
+end
+
+script 'create HDFS home directory for the vagrant user' do
+  interpreter "bash"
+  user "vagrant"
+  code <<-SCRIPT
+hadoop fs -mkdir -p /user/vagrant
+  SCRIPT
+end
